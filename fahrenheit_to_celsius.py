3404cb6450adcd58b7e145bf064c51a50ab10e18
Chris Borowski <krzysztofeborowski@gmail.com>
2023-06-09 17:42:33 -0500

    Added support for floats

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -5,7 +5,7 @@ def fahrenheit_to_celsius(temp_f):
   return round((temp_f - FREEZING_ZERO) * 5/9, 2)
 
 # Collect the user's input to be converted
-user_query = int(input("Enter the temperature in Fahrenheit: "))
+user_query = int(float(input("Enter the temperature in Fahrenheit: ")))
 print(f"You entered: {user_query} F")
 
 # Convert the user's input and print the result
